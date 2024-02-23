87eb5e05fbfdda8110acfb81ae7488d00b17a77e
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2024-02-23 11:11:29 +0900

    :gift: feat: 제목 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Head from "next/head";
 import List from "@/components/list";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center w-full h-screen">
+      <h1 className="font-mono text-3xl mb-5">근무 시간 계산기</h1>
       <section className="w-3/5 bg-neutral-100 rounded-lg p-10">
         <List />
       </section>
